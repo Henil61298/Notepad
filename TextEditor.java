ac29875d7d56308cb31f3477dfa7c59a68532fa3
Henil61298 <44196527+Henil61298@users.noreply.github.com>
2024-01-12 23:45:59 +0530

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -5,10 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class TextEditor implements ActionListener {
-//    Declaring properties of text editor
+
     JFrame frame;
     JMenuBar menuBar;
     JMenu file, edit;
